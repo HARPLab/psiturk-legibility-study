9006296d798d225c45fdc0143788488a84ac4557
Ada Taylor <ada@idu.com>
2020-08-05 15:41:37 -0400

    Added support for local running

--- data_sideAnalysis.py
+++ data_sideAnalysis.py
@@ -18,6 +18,9 @@ UNSURE_WINDOW = 5 #the amount of values around 50 that counts as people being un
 REVERSALS_WINDOW = 5 #the amount of values around 50 that don't count as the participant changing their mind. 0 = participants change their mind every time the confidence value passes 50
 RETURN_LIST_OF_AVERAGES = False #if you want the list of all the trials' averages (or total list of reversals) rather than the overall averages
 UNSUPPORTED_BROWSER_ERROR = False #for the pilot study, some people used unsupported browsers. If that's the case, this will trigger and adapt the rest of it
+
+FLAG_EXPORT = False
+FLAG_LOCAL_VERSION = True
 '''
  START: BOILER PLATE SET UP
 '''
@@ -26,56 +29,60 @@ db_url = dbkeys.db_url
 table_name = dbkeys.table_name
 data_column_name = dbkeys.data_column_name
 
+if not FLAG_LOCAL_VERSION:
+    # boilerplace sqlalchemy setup
+    engine = create_engine(db_url)
+    metadata = MetaData()
+    metadata.bind = engine
+    table = Table(table_name, metadata, autoload=True)
+    # make a query and loop through
+    s = table.select()
+    rows = s.execute()
 
-# boilerplace sqlalchemy setup
-engine = create_engine(db_url)
-metadata = MetaData()
-metadata.bind = engine
-table = Table(table_name, metadata, autoload=True)
-# make a query and loop through
-s = table.select()
-rows = s.execute()
 
+    data = []
 
-data = []
+    #status codes of subjects who completed experiment
+    statuses = [3,4,5,7]
+    # if you have workers you wish to exclude, add them here
+    exclude = []
+    for row in rows:
+        # only use subjects who completed experiment and aren't excluded
+        if row['status'] in statuses and row['uniqueid'] not in exclude:
+            data.append(row[data_column_name])
+        
+    #For use when not excluding participants:
+    #for row in rows:
+    #        data.append(row[data_column_name])
+    #        #ids.append((row['assignmentID'], condition))
+            
 
-#status codes of subjects who completed experiment
-statuses = [3,4,5,7]
-# if you have workers you wish to exclude, add them here
-exclude = []
-for row in rows:
-    # only use subjects who completed experiment and aren't excluded
-    if row['status'] in statuses and row['uniqueid'] not in exclude:
-        data.append(row[data_column_name])
-    
-#For use when not excluding participants:
-#for row in rows:
-#        data.append(row[data_column_name])
-#        #ids.append((row['assignmentID'], condition))
+    # Now we have all participant datastrings in a list.
+    # Let's make it a bit easier to work with:
+
+    # parse each participant's datastring as json object
+    # and take the 'data' sub-object
+    data = [json.loads(part)['data'] for part in data]
+
+    # insert uniqueid field into trialdata in case it wasn't added
+    # in experiment:
+    for part in data:
+        for record in part:
+            record['trialdata']['uniqueid'] = record['uniqueid']
         
 
-# Now we have all participant datastrings in a list.
-# Let's make it a bit easier to work with:
+        
+    # flatten nested list so we just have a list of the trialdata recorded
+    # each time psiturk.recordTrialData(trialdata) was called.
+    data = [record['trialdata'] for part in data for record in part]
 
-# parse each participant's datastring as json object
-# and take the 'data' sub-object
-data = [json.loads(part)['data'] for part in data]
+    # Put all subjects' trial data into a dataframe object from the
+    # 'pandas' python library: one option among many for analysis
+    df = pd.DataFrame(data)
+else:
+    df = pd.read_json('pilot-all-frames.json')
+    print("Pandas data imported from JSON")
 
-# insert uniqueid field into trialdata in case it wasn't added
-# in experiment:
-for part in data:
-    for record in part:
-        record['trialdata']['uniqueid'] = record['uniqueid']
-    
-
-    
-# flatten nested list so we just have a list of the trialdata recorded
-# each time psiturk.recordTrialData(trialdata) was called.
-data = [record['trialdata'] for part in data for record in part]
-
-# Put all subjects' trial data into a dataframe object from the
-# 'pandas' python library: one option among many for analysis
-df = pd.DataFrame(data)
 '''
  END: BOILER PLATE SET UP
 '''
